cd01caafcb8b2ba62fe6c89fdad91bac05f279bd
ppind <pearl.pind@gmail.com>
2023-11-03 12:17:07 +0200

    thumbs down

--- index.js
+++ index.js
@@ -14,9 +14,12 @@ fetch('json/jason.json')
             for (var i = 0; i < posts.length; i++) {
                 var post = posts[i];
                 var userIcon = post.userIcon;
+                var userName = post.user;
                 var title = post.title;
                 var date = post.date;
                 var text = post.text;
+                var dislikes = post.dislikes;
+
 
                 var postDiv = document.createElement('div');
                 postDiv.setAttribute('class', 'post');
@@ -24,29 +27,48 @@ fetch('json/jason.json')
                 postHeaderDiv.setAttribute('class', 'postHeader');
                 var postContentDiv = document.createElement('div');
                 postContentDiv.setAttribute('class', 'postContent');
+                var postDislikesDiv = document.createElement('div');
+                postDislikesDiv.setAttribute('class', 'postDislikes');
+
+                if (post.isPinned) {
+                    var postPinnedDiv = document.createElement('div');
+                    postPinnedDiv.setAttribute('class', 'postPinned');
+                    postDiv.appendChild(postPinnedDiv);
+
+                    var pinnedH = document.createElement('h1');
+                    pinnedH.setAttribute('class', 'pinned title');
+                    pinnedH.textContent = 'PINNED: ';
+                    postPinnedDiv.appendChild(pinnedH);
+                }
                 postDiv.appendChild(postHeaderDiv);
                 postDiv.appendChild(postContentDiv);
+                postDiv.appendChild(postDislikesDiv);
+
+                
 
                 var userIconImg = document.createElement('img');
                 userIconImg.setAttribute('class', 'userIcon');
                 userIconImg.setAttribute('src', userIcon);
                 userIconImg.setAttribute('alt', 'guy');
-                var titleH = document.createElement('h1');
-                titleH.setAttribute('class', 'title');
-                titleH.textContent = title;
+
+                var userNameP = document.createElement('p');
+                userNameP.setAttribute('class', 'userName');
+                userNameP.textContent = userName;
+
                 var dateH = document.createElement('h3');
                 dateH.setAttribute('class', 'date');
                 dateH.textContent = ISO8601ToText(date);
                 postHeaderDiv.appendChild(userIconImg);
-                if (post.isPinned) {
-                    var pinnedH = document.createElement('h1');
-                    pinnedH.setAttribute('class', 'pinned title');
-                    pinnedH.textContent = 'PINNED: ';
-                    postHeaderDiv.appendChild(pinnedH);
-                }
-                postHeaderDiv.appendChild(titleH);
-                postHeaderDiv.appendChild(dateH);
                 
+                postHeaderDiv.appendChild(userNameP);
+                postHeaderDiv.appendChild(dateH);
+
+                
+                var titleH = document.createElement('h1');
+                titleH.setAttribute('class', 'title');
+                titleH.textContent = title;
+                postContentDiv.appendChild(titleH);
+
                 if (post.postImage.length > 2) {
                     var postImageImg = document.createElement('img');
                     postImageImg.setAttribute('class', 'postImage');
@@ -58,7 +80,19 @@ fetch('json/jason.json')
                 var postTextP = document.createElement('p');
                 postTextP.setAttribute('class', 'postText');
                 postTextP.textContent = text;
+                
                 postContentDiv.appendChild(postTextP);
+
+                var postDislikesP = document.createElement('p');
+                postDislikesP.setAttribute('class', 'postDislikesP');
+                postDislikesP.textContent = dislikes;
+                
+                var postDislikesImg = document.createElement('img');
+                postDislikesImg.setAttribute('class', 'dislikesImg');
+                postDislikesImg.setAttribute('src', "vudinad_ja_vidinad/dislike.png");
+                postDislikesImg.setAttribute('alt', 'dislikes');
+                postDislikesDiv.appendChild(postDislikesImg)
+                postDislikesDiv.appendChild(postDislikesP);
 
                 if (post.isPinned) {
                     main.insertBefore(postDiv, main.firstChild);
