c28f98634fda7305fbafcb782083ace09243b9c7
ppind <pearl.pind@gmail.com>
2023-12-16 22:15:07 +0200

    backend?

--- backend/database.js
+++ backend/database.js
@@ -5,7 +5,36 @@
      password: "paroolfr",
      database: "basedAH",// ma ei tea kuidas andmebaasid töötavad btw
      host: "localhost", 
-     port: "5432"
+     port: "5433"
  });
  
- module.exports = pool;
+ const execute = async(query) => {
+    try {
+        await pool.connect(); // create a connection
+        await pool.query(query); // executes a query
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    }
+};
+
+/* 
+gen_random_uuid() A system function to generate a random Universally Unique IDentifier (UUID)
+An example of generated uuid:  32165102-4866-4d2d-b90c-7a2fddbb6bc8
+*/
+
+const createTblQuery = `
+    CREATE TABLE IF NOT EXISTS "users" (
+        id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+        email VARCHAR(200) NOT NULL UNIQUE,
+        password VARCHAR(200) NOT NULL 
+    );`;
+
+execute(createTblQuery).then(result => {
+    if (result) {
+        console.log('Table "users" is created');
+    }
+});
+
+module.exports = pool;
