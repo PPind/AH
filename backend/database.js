e54ed02b15c2e9b09d64e83135c9f4dbf7a5f6df
Marlene Ibrus <marlene.ibrus@gmail.com>
2023-12-18 01:10:23 +0200

    mingi muudatusxd

--- backend/database.js
+++ backend/database.js
@@ -2,17 +2,19 @@
  const Pool = require('pg').Pool;
  const pool = new Pool({
      user: "postgres",
-     password: "3l3v4nd1Mees",
+     password: "M42rru",
      database: "basedAH",// ma ei tea kuidas andmebaasid töötavad btw
      host: "localhost", 
      port: "5432"
 
  });
  
- const execute = async(query) => {
+ const execute = async(createTblQuery1, createTblQuery2, addPostsQuery) => {
     try {
         await pool.connect(); // gets connection
-        await pool.query(query); // sends queries
+        await pool.query(createTblQuery1); // sends queries
+        await pool.query(createTblQuery2);
+        await pool.query(addPostsQuery);
         return true;
     } catch (error) {
         console.error(error.stack);
@@ -29,21 +31,21 @@ const createTblQuery1 = `
     CREATE TABLE IF NOT EXISTS "posttable" (
 	    "id" SERIAL PRIMARY KEY,
 	    "title" VARCHAR(200) NOT NULL,
-	    "text" VARCHAR(200) NOT NULL,
+	    "body" VARCHAR(200) NOT NULL,
         "dislikes" INT NOT NULL,
         "isPinned" BOOLEAN NOT NULL,
-        "date" CHAR(10) NOT NULL
+        "date" DATE NOT NULL
     );`;
 
 const createTblQuery2 = `
     CREATE TABLE IF NOT EXISTS "users" (
-        id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-        email VARCHAR(200) NOT NULL UNIQUE,
-        password VARCHAR(200) NOT NULL 
+        "id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+        "email" VARCHAR(200) NOT NULL UNIQUE,
+        "password" VARCHAR(200) NOT NULL 
     );`;
 
 const addPostsQuery = `
-    INSERT INTO posttable (title, text, dislikes, isPinned, date)
+    INSERT INTO posttable (title, body, dislikes, isPinned, date)
     VALUES
     ('cheeze', 'rat eat cheese i think', 5, false, '2006-10-23'),
     ('hiii', '"MA OLEN VÄIKE SILLY KASS KES ELAB SU PEA SEES IHHIHIHIHIH', 160549, true, '2023-10-01'),
@@ -51,24 +53,12 @@ const addPostsQuery = `
     `
 
 // A function to execute the previous query   
-execute(createTblQuery1).then(result => {
+execute(createTblQuery1, createTblQuery2, addPostsQuery).then(result => {
     if (result) {
-        console.log('If does not exists, table "posttable" is created');
+        console.log('If does not exists, table "posttable" and "users" are created');
     }
 });
 
-// A function to execute the previous query   
-execute(createTblQuery2).then(result => {
-    if (result) {
-        console.log('If does not exists, create the "posttable" table');
-    }
-});
-
-execute(addPostsQuery).then(result => {
-    if (result) {
-        console.log('Adds a few sample posts to the database');
-    }
-});
 
 
 module.exports = pool;
--- frontend/src/views/AddpostView.vue
+++ frontend/src/views/AddpostView.vue
@@ -30,7 +30,6 @@
             post: {
                 title: "",
                 body: "",
-                urllink: "",
         },
         }
     },
@@ -42,7 +41,6 @@
         var data = {
           title: this.post.title,
           body: this.post.body,
-          urllink: this.post.urllink,
         };
         // using Fetch - post method - send an HTTP post request to the specified URI with the defined body
         fetch("http://localhost:3000/api/posts", {
