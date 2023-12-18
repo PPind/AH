788f54331def350eeaead1452764fa5d992a4b27
Marlene Ibrus <marlene.ibrus@gmail.com>
2023-12-18 02:05:12 +0200

    postitused

--- backend/database.js
+++ backend/database.js
@@ -9,11 +9,12 @@
 
  });
  
- const execute = async(query1, query2) => {
+ const execute = async(query1, query2, insertDataQuery) => {
     try {
         await pool.connect(); // gets connection
         await pool.query(query1); // sends queries
         await pool.query(query2); // sends queries
+        await pool.query(insertDataQuery);
         return true;
     } catch (error) {
         console.error(error.stack);
@@ -36,10 +37,21 @@ const createTblQuery2 = `
         "password" VARCHAR(200) NOT NULL 
     );`;
 
-
+const insertDataQuery = `WITH data (title, body, date) AS 
+    (
+     VALUES
+     ('Heeyyy', 'MA OLEN UUS VÄIKE KASSIKENE KES ELAB SU PEAS IHIHIHIIHIH', TO_DATE('2022-03-24', 'YYYY-MM-DD')),  
+     ('sÄGA', 'mulle väga meeldib SÄGA', TO_DATE('2022-03-24', 'YYYY-MM-DD')),
+     ('Good hat cheap price', 'wher has hatmann gon i mizz him ', TO_DATE('2023-03-24', 'YYYY-MM-DD'))
+     )
+   INSERT INTO posttable(title, body, date) 
+   SELECT  d.title, d.body, d.date
+   FROM data d
+   WHERE NOT EXISTS (SELECT * FROM posttable WHERE id = 1);
+ `
 
 // A function to execute the previous query   
-execute(createTblQuery1, createTblQuery2).then(result => {
+execute(createTblQuery1, createTblQuery2, insertDataQuery).then(result => {
     if (result) {
         console.log('If does not exists, table "users" and  table "posttable" are created');
     }
